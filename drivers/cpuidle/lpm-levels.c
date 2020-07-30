1eedf30258fdad102ecf7acf6a0c533c9f8b8cf7
Danny Lin <danny@kdrag0n.dev>
2020-07-30 00:55:57 -0700

    cpuidle: lpm-levels: Ignore sleep_disabled boot parameter
    Disabling cpuidle makes the CPU heat up (obviously), and since we'll be
    running a custom minimal Alpine Linux userspace that doesn't write to
    sleep_disabled, let's just remove this knob for now.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -105,7 +105,6 @@ static bool print_parsed_dt;
 module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 
 static bool sleep_disabled;
-module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
 
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
